d9b3fb18d2a019b480f4a137787202492a74d135
lindo315 <lindodlamini315@gmail.com>
2024-08-26 16:16:48 +0200

    welcome edit

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 function HomePage({ userRole }) {
-	return (
-		<div>
-			<h2>Welcome to the Home Page</h2>
-			{userRole === "student" && <p>This content is for students.</p>}
-			{userRole === "admin" && <p>This content is for admins.</p>}
-		</div>
-	);
+  return (
+    <div>
+      <h2>Welcome Home Page</h2>
+      {userRole === "student" && <p>This content is for students.</p>}
+      {userRole === "admin" && <p>This content is for admins.</p>}
+    </div>
+  );
 }
 export default HomePage;
