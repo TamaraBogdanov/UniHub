db73617c7485ffe746bd8d4231877563ef3d604b
lindo315 <lindodlamini315@gmail.com>
2024-08-26 16:16:20 +0200

    login: to login

--- src/Login/LoginPage.js
+++ src/Login/LoginPage.js
@@ -2,39 +2,39 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function LoginPage({ setUserRole }) {
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-	const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-	const handleLogin = () => {
-		if (username === "student" && password === "studentpass") {
-			setUserRole("student");
-			navigate("/home");
-		} else if (username === "admin" && password === "adminpass") {
-			setUserRole("admin");
-			navigate("/home");
-		} else {
-			alert("Invalid credentials");
-		}
-	};
+  const handleLogin = () => {
+    if (username === "student" && password === "studentpass") {
+      setUserRole("student");
+      navigate("/home");
+    } else if (username === "admin" && password === "adminpass") {
+      setUserRole("admin");
+      navigate("/home");
+    } else {
+      alert("Invalid credentials");
+    }
+  };
 
-	return (
-		<div>
-			<h2>Login: </h2>
-			<input
-				type="text"
-				placeholder="Username"
-				value={username}
-				onChange={(e) => setUsername(e.target.value)}
-			/>
-			<input
-				type="password"
-				placeholder="Password"
-				value={password}
-				onChange={(e) => setPassword(e.target.value)}
-			/>
-			<button onClick={handleLogin}>Login</button>
-		</div>
-	);
+  return (
+    <div>
+      <h2>Login </h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
 }
 export default LoginPage;
