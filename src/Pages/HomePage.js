5ad48e6651089b61bb4a252b218e86eff7474656
MilkMother <118103942+MilkMother@users.noreply.github.com>
2024-08-27 18:30:45 +0200

    added a topbar

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Topbar from "../Shared/CTopBar";
 
 function HomePage({ userRole }) {
 	return (
-		<div>
-			<h2>Welcome to the Home Page</h2>
-			{userRole === "student" && <p>This content is for students.</p>}
-			{userRole === "admin" && <p>This content is for admins.</p>}
+		<div className="HomePage">
+			{/* Added a topbar that changes depending on which page youre on and which user youre logged in as */}
+			<Topbar currentPage={"Dashboard"} userRole={userRole} />
+			<div>
+				<h2>Welcome to the Dashboard</h2>
+				{userRole === "student" && <p>This content is for students.</p>}
+				{userRole === "admin" && <p>This content is for admins.</p>}
+			</div>
 		</div>
 	);
 }
