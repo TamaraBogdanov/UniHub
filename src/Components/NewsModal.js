3a9f73d8efe21fa8363159f537cc81e2c506608a
MilkMother <118103942+MilkMother@users.noreply.github.com>
2024-10-14 01:57:36 +0200

    tweak

--- src/Components/NewsModal.js
+++ src/Components/NewsModal.js
@@ -2,27 +2,27 @@ import React from "react";
 import { X } from "lucide-react";
 
 function NewsModal({ news, onClose }) {
-  if (!news) return null;
+	if (!news) return null;
 
-  return (
-    <div className="news-modal-overlay">
-      <div className="news-modal">
-        <button className="close-button" onClick={onClose}>
-          <X size={24} />
-        </button>
-        <img src={news.image} alt={news.title} className="modal-image" />
-        <div className="modal-content">
-          <h2>{news.title}</h2>
-          <p className="modal-meta">
-            {news.category} - {news.subcategory} | {news.date}
-          </p>
-          <p className="modal-author">By {news.author}</p>
-          <p className="modal-publisher">Published by {news.publisher}</p>
-          <div className="modal-body">{news.content}</div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="news-modal-overlay">
+			<div className="news-modal">
+				<button className="close-button" onClick={onClose}>
+					<X size={24} />
+				</button>
+				<img src={news.image} alt={news.title} className="modal-image" />
+				<div className="modal-content">
+					<h2>{news.title}</h2>
+					<p className="modal-meta">
+						{news.category} - {news.subcategory} | {news.date}
+					</p>
+					<p className="modal-author">By {news.author}</p>
+					<p className="modal-publisher">Published by {news.publisher}</p>
+					<div className="modal-body">{news.content}</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default NewsModal;
